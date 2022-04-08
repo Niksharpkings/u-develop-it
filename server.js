a1697c960c3ac61b122a26502f6db02f8e5a3ab3
Niksharpkings <67552318+Niksharpkings@users.noreply.github.com>
2022-04-07 22:45:07 -0700

    12.2.3

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
+//import express
 const express = require('express');
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Waddup local World?'});
+
+//404 status
+app.use((req, res) => { 
+    res.status(404).json({
+        message: '404 Page Not Found'
+    });
 });
 
+//express server port listener
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
+
+ 
